a264a84e59789687249ccd393ac2f5c794b0bc1f
jeremih <jeremih.odiwuor@gmail.com>
2025-08-20 23:19:56 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SitemapController;
-use Filament\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/football-predictions', function () {
-    return view('football-predictions');
-});
-
-Route::get('/soccer-betting-tips', function () {
-    return view('soccer-betting-tips');
-});
-
-Route::get('/vip-soccer-betting-tips', function () {
-    return view('vip-soccer-betting-tips');
-});
-
-Route::get('/sitemap.xml', [SitemapController::class, 'index']);
-
-// Admin routes
-Route::name('filament.')->group(function () {
-    Route::get('admin/login', [LoginController::class, 'create'])->name('auth.login');
-    Route::post('admin/login', [LoginController::class, 'store']);
+    return view('welcome');
 });
