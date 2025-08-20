d89fc6f6b706bd3e9451c15411fb0a66dde0ec3f
jeremih <jeremih.odiwuor@gmail.com>
2025-08-20 23:40:00 +0300

    Add Filament auth POST route

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SitemapController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/football-predictions', function () {
-    return view('football-predictions');
-});
-
-Route::get('/soccer-betting-tips', function () {
-    return view('soccer-betting-tips');
-});
-
-Route::get('/vip-soccer-betting-tips', function () {
-    return view('vip-soccer-betting-tips');
-});
-
-Route::get('/sitemap.xml', [SitemapController::class, 'index']);
+// Filament auth routes
+Route::post('/admin/login', function () {
+    return app(\Filament\Http\Responses\Auth\LoginResponse::class);
+})->middleware(['web'])->name('filament.admin.auth.login.store');
